0d00b3d42ffb9b72618b2d58e5e5b33618aefa1b
Luisa VD <35203863+luisavdornelles@users.noreply.github.com>
2025-05-25 12:58:48 -0400

    E-Commerce Setup (#3)
    * setting up initial layout
    
    * Improving code reusability
    
    * Install Sass
    
    * Add categories styles
    
    * Moving data to JSON file
    
    * Creating CategoryItem component
    
    * Creating Directory component
    
    * Installing Open Sans font

---
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -0,0 +1,18 @@
+import "./directory.styles.scss";
+import CategoryItem from "../category-item/category-item.component";
+
+const Directory = ({ categories }) => {
+    return (
+        <div className="directory-container">
+            {categories.map(({ title, id, imageUrl }) => (
+                <CategoryItem
+                    key={`${id}-${title}`}
+                    title={title}
+                    imageUrl={imageUrl}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default Directory;
