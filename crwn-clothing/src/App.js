cbcc51be2859b10663975f533350f853857f7bef
Luisa Vargas <luisavdornelles@gmail.com>
2025-05-25 12:33:57 -0400

    Creating CategoryItem component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,17 +1,16 @@
 import "./categories.styles.scss";
 import categories from "./categories.json";
+import CategoryItem from "./components/category-item/category-item.component";
 
 const App = () => {
     return (
         <div className="categories-container">
-            {categories.map(({ title, id }) => (
-                <div key={`${id}-${title}`} className="category-container">
-                    <div className="background-image" />
-                    <div className="category-body-container">
-                        <h2>{title}</h2>
-                        <p>Shop Now</p>
-                    </div>
-                </div>
+            {categories.map(({ title, id, imageUrl }) => (
+                <CategoryItem
+                    key={`${id}-${title}`}
+                    title={title}
+                    imageUrl={imageUrl}
+                />
             ))}
         </div>
     );
