c928f7b21282aa2a1536594d4ec03e40483ca6fd
Luisa Vargas <luisavdornelles@gmail.com>
2025-05-25 12:05:24 -0400

    Improving code reusability

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,36 +1,38 @@
 const App = () => {
+    const categories = [
+        {
+            id: 1,
+            title: "Hats",
+        },
+        {
+            id: 2,
+            title: "Jackets",
+        },
+        {
+            id: 3,
+            title: "Sneakers",
+        },
+        {
+            id: 4,
+            title: "Womens",
+        },
+        {
+            id: 5,
+            title: "Mens",
+        },
+    ];
+
     return (
         <div className="categories-container">
-            <div className="category-container">
-                <div className="category-body">
-                    <h2>Hats</h2>
-                    <p>Shop Now</p>
+            {categories.map(({ title, id }) => (
+                <div key={`${id}-${title}`} className="category-container">
+                    <div className="background-image" />
+                    <div className="category-body">
+                        <h2>{title}</h2>
+                        <p>Shop Now</p>
+                    </div>
                 </div>
-            </div>
-            <div className="category-container">
-                <div className="category-body">
-                    <h2>Jackets</h2>
-                    <p>Shop Now</p>
-                </div>
-            </div>
-            <div className="category-container">
-                <div className="category-body">
-                    <h2>Sneakers</h2>
-                    <p>Shop Now</p>
-                </div>
-            </div>
-            <div className="category-container">
-                <div className="category-body">
-                    <h2>Tops</h2>
-                    <p>Shop Now</p>
-                </div>
-            </div>
-            <div className="category-container">
-                <div className="category-body">
-                    <h2>Bottoms</h2>
-                    <p>Shop Now</p>
-                </div>
-            </div>
+            ))}
         </div>
     );
 };
