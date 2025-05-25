f2e44bb9945e735a7bc3c90f4c1849562f45a967
Luisa Vargas <luisavdornelles@gmail.com>
2025-05-25 12:33:40 -0400

    Moving data to JSON file

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,29 +1,7 @@
 import "./categories.styles.scss";
+import categories from "./categories.json";
 
 const App = () => {
-    const categories = [
-        {
-            id: 1,
-            title: "Hats",
-        },
-        {
-            id: 2,
-            title: "Jackets",
-        },
-        {
-            id: 3,
-            title: "Sneakers",
-        },
-        {
-            id: 4,
-            title: "Womens",
-        },
-        {
-            id: 5,
-            title: "Mens",
-        },
-    ];
-
     return (
         <div className="categories-container">
             {categories.map(({ title, id }) => (
